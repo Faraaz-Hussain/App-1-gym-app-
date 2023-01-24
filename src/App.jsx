8a5a8b5d9dc02b25100c43a75669af70ef0a8a18
mdsufiyan2210 <mdsufiyan2210@gmail.com>
2023-01-24 14:08:32 +0530

    login1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div>
-      <div className='background-image'></div>
-      <h1 className='content'>hello</h1>
+     {/* <Login/> */}
     </div>
   );
 }
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./login.css";
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="image"></div>
+      <div className="text-box">
+        <TextField  InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }} style={{marginLeft:"40px",marginTop:"50px"}} id="outlined-basic" label="username" variant="standard" />
+        <TextField InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }} style={{marginLeft:"40px",marginTop:"30px"}} id="outlined-basic" label="password" variant="standard" type="password" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
